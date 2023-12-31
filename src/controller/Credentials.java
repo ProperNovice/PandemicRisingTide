673ac6c841c6fa58eb875d2ff8603a83af0ebb0f
ProperNovice <ProperNovice@users.noreply.github.com>
2023-12-31 19:05:11 +0200

    Update Credentials.java

--- src/controller/Credentials.java
+++ src/controller/Credentials.java
@@ -74,6 +74,22 @@ public enum Credentials {
 		y = 40;
 		this.dWaterCube = new Vector2(40, 40);
 
+		// d dike
+
+		this.dDike = this.dWaterCube.clone();
+
+		// d water pump
+
+		y = 60;
+		x = 86 * y / 100;
+		this.dWaterPump = new Vector2(x, y);
+
+		// d port
+
+		y = 60;
+		x = 64 * y / 100;
+		this.dPort = new Vector2(x, y);
+
 		// c water cubes
 
 		x = this.gapBetweenBorders;
@@ -82,33 +98,17 @@ public enum Credentials {
 		y = this.gapBetweenBorders;
 		this.cWaterCubes = new Vector2(x, y);
 
-		// d dike
-
-		this.dDike = this.dWaterCube.clone();
-
 		// c dikes
 
 		this.cDikes = this.cWaterCubes.clone();
 		this.cDikes.y += this.dWaterCube.y;
 		this.cDikes.y += this.dGapBetweenComponents.y;
 
-		// d water pump
-
-		x = 86;
-		y = 100;
-		this.dWaterPump = new Vector2(x, y);
-
 		// c water pumps
 
 		this.cWaterPumps = this.cDikes.clone();
 		this.cWaterPumps.y += this.dDike.y;
 		this.cWaterPumps.y += this.dGapBetweenComponents.y;
-
-		// d port
-
-		x = 86;
-		y = 100;
-		this.dPort = new Vector2(x, y);
 
 		// c ports
 
