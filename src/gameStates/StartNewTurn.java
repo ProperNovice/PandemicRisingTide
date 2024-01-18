5fd9cc4bb68c8cdfe2686a35e5a0f78f3f91e446
ProperNovice <ProperNovice@users.noreply.github.com>
2024-01-18 19:10:21 +0200

    Update StartNewTurn.java

--- src/gameStates/StartNewTurn.java
+++ src/gameStates/StartNewTurn.java
@@ -1,5 +1,6 @@
 package gameStates;
 
+import functions.Flood;
 import functions.SelectPumpingStationsAvailableToOperate;
 import functions.SkipDikesFailStepThisTurn;
 import gameStatesDefault.GameState;
@@ -23,6 +24,7 @@ public class StartNewTurn extends GameState {
 		}
 
 		SkipDikesFailStepThisTurn.INSTANCE.execute();
+		Flood.INSTANCE.clearList();
 
 	}
 
